8b40bf437f4725a2f8e2a48fd320d38b72c87520
KupriyanovP <kupriyanov3001@mail.ru>
2019-10-18 15:08:36 +0300

    add table

--- shar.py
+++ shar.py
@@ -16,7 +16,10 @@ def new_ball():
     obj_coords.append([x, y, r, vx, vy])
     a = canv.create_oval(x - r, y - r, x + r, y + r, fill=choice(colors), width=0)
     l_obj.append(a)
-    root.after(3000, new_ball)
+    if time.perf_counter()-time1>60:
+        table()
+        exit()
+    root.after(2000, new_ball)
 
 
 def motion():
@@ -49,20 +52,65 @@ def click(event):
         obj_coords.pop(j)
         l_obj.pop(j)
 
+def restart(event):
+    global obj_coords, l_obj, score, score_text
+    canv.delete(ALL)
+    l_obj = []
+    obj_coords = []
+    score = 0
+    score_text = canv.create_text(2, 0, text="Your Score: " + str(score), anchor=NW, font="Verdana 14")
 
+def table():
+    global name,score
+    #read table
+    f = open("table.txt", 'r', encoding='utf-8')
+    l=f.readlines()
+    f.close()
+    flag=0
+    for i in range(len(l)):
+        l[i]=list(l[i].split())
+    for i in range(len(l)):
+        if (l[i][0]==name):
+            flag=1
+            if int(l[i][1])<score:
+                l[i][1]=str(score)
+    #add new player
+    if flag==0:
+        l.append([name, str(score)])
+    #write new table
+    f = open("table.txt", 'w', encoding='utf-8')
+    for i in range(len(l)):
+        f.write(l[i][0]+' '+l[i][1]+'\n')
+    f.close()
+
+def timer():
+    global seconds, timer_text
+    seconds+=1
+    canv.delete(timer_text)
+    timer_text = canv.create_text(800, 600, text="Time:" + str(seconds), anchor=SE, font="Verdana 14")
+    root.after(1000, timer)
+
+name=input("Enter your name: ")
+
+#canvas
 root = Tk()
 root.geometry('800x600')
-
 canv = Canvas(root, bg='white')
 canv.pack(fill=BOTH, expand=1)
 colors = ['red', 'orange', 'yellow', 'green', 'blue']
 
-l1 = []
 l_obj = []
 obj_coords = []
 score = 0
+seconds=0
 score_text = canv.create_text(2, 0, text="Your Score: " + str(score), anchor=NW, font="Verdana 14")
+timer_text=canv.create_text(800, 600, text="Time:" + str(seconds), anchor=SE, font="Verdana 14")
+time1=time.perf_counter()
 new_ball()
 motion()
+timer()
 canv.bind('<Button-1>', click)
+canv.bind_all('<space>', restart)
 mainloop()
+
+table()
